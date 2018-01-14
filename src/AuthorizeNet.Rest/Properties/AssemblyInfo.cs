0463e52b70795835d324ad0cdf5fa5826a579f59
sunnyrajrathod <sunnyrajrathod@gmail.com>
2018-01-14 17:52:45 +0530

    - Manually changed AssemblyInfo.cs, since its not supported by swagger template yet.

--- src/AuthorizeNet.Rest/Properties/AssemblyInfo.cs
+++ src/AuthorizeNet.Rest/Properties/AssemblyInfo.cs
@@ -4,12 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Swagger Library")]
-[assembly: AssemblyDescription("A library generated from a Swagger doc")]
+[assembly: AssemblyTitle("Authorize.Net REST Library")]
+[assembly: AssemblyDescription("A library for Authorize.NET REST API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Swagger")]
-[assembly: AssemblyProduct("SwaggerLibrary")]
-[assembly: AssemblyCopyright("No Copyright")]
+[assembly: AssemblyCompany("AuthorizeNet")]
+[assembly: AssemblyProduct("AuthorizeNet.REST")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
