313abf5f994e8f4ae1e62d27321c63e3f1aba3ea
sunnyrajrathod <sunnyrajrathod@gmail.com>
2018-01-14 17:09:23 +0530

    - Adding template to the specific generator folders.

--- generator/authorize_csharp_sdk_gen.bat
+++
@@ -1,1 +0,0 @@
-java -jar swagger-codegen-cli-2.2.3.jar generate -i anet-rest-spec.json -t .\swagger-codegen\modules\swagger-codegen\src\main\resources\authorizenet-csharp -l csharp -o ..\ -c authorizenet-csharp-config.json -DmodelTests=false
---
+++ generator/authorizenet_csharp_sdk_gen.bat
@@ -0,0 +1,1 @@
+java -jar swagger-codegen-cli-2.2.3.jar generate -i anet-rest-spec.json -t .\authorizenet-csharp -l csharp -o ..\ -c authorizenet-csharp-config.json -DmodelTests=false
